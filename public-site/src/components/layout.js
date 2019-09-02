489814a10732ffaf5121094646f141d2ef255653
David Poindexter <drpoindexter@gmail.com>
2019-09-02 12:58:23 -0400

    Create styled-component in layout.js

--- public-site/src/components/layout.js
+++ public-site/src/components/layout.js
@@ -8,9 +8,17 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import styled from "styled-components"
 
 import Header from "./header"
-import GlobalStyle from './globalStyle.js'
+import GlobalStyle from "./globalStyle.js"
+
+const Container = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0px 1.0875rem 1.45rem;
+  padding-top: 0;
+`
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -21,28 +29,20 @@ const Layout = ({ children }) => {
         }
       }
     }
-  `);
-
+  `)
 
   return (
     <>
       <GlobalStyle />
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
+      <Container>
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()} David Poindexter, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
-      </div>
+      </Container>
     </>
   )
 }
