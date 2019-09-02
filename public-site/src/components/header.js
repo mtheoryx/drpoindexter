20e009d47bb9f5ea8e20fef32b52c4b4bc19633d
David Poindexter <drpoindexter@gmail.com>
2019-09-02 14:59:29 -0400

    Remove css from inline styles

--- public-site/src/components/header.js
+++ public-site/src/components/header.js
@@ -1,36 +1,50 @@
 import React from "react"
+import styled from "styled-components"
 import { Link } from "gatsby"
 
 import Navigation from "./navigation"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-      <Navigation />
-    </div>
-  </header>
+const SiteHeading1 = ({ className, children }) => (
+  <h1 className={className}>{children}</h1>
 )
 
-export default Header
+const StyledSiteHeading1 = styled(SiteHeading1)`
+  margin: 0;
+`
+
+const HeaderWrapper = ({ className, children }) => (
+  <div className={className}>{children}</div>
+)
+
+const StyledHeaderWrapper = styled(HeaderWrapper)`
+  margin: 0 auto;
+  max-width: 960;
+  padding: 1.45rem 1.0875rem;
+`
+
+const Header = ({ className, children }) => (
+  <header className={className}>{children}</header>
+)
+
+const StyledHeader = styled(Header)`
+  background: rebeccapurple;
+  margin-bottom: 1.45rem;
+`
+
+const StyledLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+`
+
+const HeaderComponent = ({ siteTitle }) => (
+  <StyledHeader>
+    <StyledHeaderWrapper>
+      <StyledSiteHeading1>
+        <StyledLink to="/">{siteTitle}</StyledLink>
+      </StyledSiteHeading1>
+      <Navigation />
+    </StyledHeaderWrapper>
+  </StyledHeader>
+)
+
+export default HeaderComponent
