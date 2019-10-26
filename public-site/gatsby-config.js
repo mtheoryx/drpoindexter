28b922e674ff4b9aa88f4e8acc3e077e9823dd61
David Poindexter <drpoindexter@gmail.com>
2019-10-26 14:57:45 -0400

    Add styled componets to reduce FOUC

--- public-site/gatsby-config.js
+++ public-site/gatsby-config.js
@@ -72,6 +72,8 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
