86d11cfc30bad5da2cb0e078e3fc6baea622c140
David Poindexter <drpoindexter@gmail.com>
2019-12-30 17:37:25 -0500

    Responsive footer

--- public-site/src/components/footer.js
+++ public-site/src/components/footer.js
@@ -1,51 +1,87 @@
 import React from "react"
-// import styled from "styled-components"
+import styled from "styled-components"
 
-const Footer = () => (
-  <footer>
-    <a
-      href="https://media.giphy.com/media/PJoLp4gDIqjYs/giphy.gif"
-      target="_blank"
-      rel="noopener noreferrer"
-      style={{ color: "#663399" }}
-    >
-      © {new Date().getFullYear()}
-    </a>{" "}
-    <a
-      href="https://www.dpoindexter.com/"
-      style={{ color: "#663399" }}
-      rel="noopener noreferrer"
-    >
-      David Poindexter
-    </a>{" "}
-    |{` `}
-    <a
-      href="https://www.gatsbyjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-      style={{ color: "#663399" }}
-    >
-      Built with Gatsby
-    </a>{" "}
-    |{" "}
-    <a
-      href="https://www.gatsbyjs.org/docs/winning-over-developers/"
-      target="_blank"
-      rel="noopener noreferrer"
-      style={{ color: "#663399" }}
-    >
-      My Website is Faster than Yours!
-    </a>{" "}
-    |{" "}
-    <a
-      href="https://aws.amazon.com/amplify/"
-      target="_blank"
-      rel="noopener noreferrer"
-      style={{ color: "#663399" }}
-    >
-      AWS Amplify
-    </a>
-  </footer>
-)
+const StyledFooter = styled.footer`
+  a {
+    margin-right: 3px;
+  }
+  a:not(:first-child) {
+    margin-right: 0;
+  }
+  .essential2,
+  .essential3 {
+    display: none;
+  }
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: center;
+
+  @media (min-width: 550px) {
+    justify-content: space-between;
+    .essential2 {
+      display: inline;
+    }
+  }
+
+  @media (min-width: 800px) {
+    justify-content: space-between;
+    .essential2,
+    .essential3 {
+      display: inline;
+    }
+  }
+`
+
+const Footer = () => {
+  return (
+    <StyledFooter>
+      <span className="essential1">
+        <a
+          href="https://media.giphy.com/media/PJoLp4gDIqjYs/giphy.gif"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ color: "#663399" }}
+        >
+          © {new Date().getFullYear()}
+        </a>
+        <a
+          href="https://www.dpoindexter.com/"
+          style={{ color: "#663399" }}
+          rel="noopener noreferrer"
+        >
+          David Poindexter
+        </a>
+      </span>
+
+      <a
+        href="https://www.gatsbyjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ color: "#663399" }}
+        className="essential2"
+      >
+        Built with Gatsby
+      </a>
+      <a
+        href="https://www.gatsbyjs.org/docs/winning-over-developers/"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ color: "#663399" }}
+        className="essential3"
+      >
+        My Website is Faster than Yours!
+      </a>
+      <a
+        href="https://aws.amazon.com/amplify/"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ color: "#663399" }}
+        className="essential2"
+      >
+        AWS Amplify
+      </a>
+    </StyledFooter>
+  )
+}
 
 export default Footer
