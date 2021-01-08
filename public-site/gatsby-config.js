62abccef9d7caa793bf88857c2b97f952ed019ae
David Poindexter <drpoindexter@gmail.com>
2021-01-08 15:28:06 -0500

    Another attempt at new image

--- public-site/gatsby-config.js
+++ public-site/gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     siteUrl: `https://www.dpoindexter.com`,
     url: `https://www.dpoindexter.com`,
     twitterUserName: "@drpoindexter",
-    image: "/images/og-image.png",
+    image: "/images/og-imagev2.png",
     navLinks: [
       { name: "devtips", link: "/devtips", published: true },
       { name: "notes", link: "/notes", published: true },
