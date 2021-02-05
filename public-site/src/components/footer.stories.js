798bd908717f2d7a4e35516c3c6b54d2ee012825
David Poindexter <drpoindexter@gmail.com>
2021-02-05 12:02:10 -0500

    Add initial storybook config

---
+++ public-site/src/components/footer.stories.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Footer from "./footer"
+
+export default {
+  title: "Dashboard/footer",
+}
+
+export const footerLight = () => (
+  <Footer />
+  // <div style={{ padding: "16px", backgroundColor: "#eeeeee" }}>
+  //   <h1 style={{ color: "rebeccapurple" }}>Hello from Storybook and Gatsby!</h1>
+  //   <Footer />
+  // </div>
+)
