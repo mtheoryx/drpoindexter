c8887f0436b252c9d8b3e53af1182f32a0a435fe
David Poindexter <drpoindexter@gmail.com>
2021-02-08 16:34:15 -0500

    Remove unused typography settings

--- public-site/src/utils/typography.js
+++ public-site/src/utils/typography.js
@@ -1,5 +1,5 @@
 import Typography from "typography"
-import oceanBeachTheme from "typography-theme-ocean-beach"
+// import oceanBeachTheme from "typography-theme-ocean-beach"
 
 // @BEGIN: Typography Customizations
 // grandViewTheme.headerFontFamily = ["Source Serif Pro", "georgia", "serif"]
@@ -10,24 +10,9 @@ import oceanBeachTheme from "typography-theme-ocean-beach"
 // grandViewTheme.bodyGray = "slate"
 // grandViewTheme.bodyColor = "slate"
 // grandViewTheme.headerColor = "slate"
-oceanBeachTheme.linkColor = "#663399"
-oceanBeachTheme.overrideThemeStyles = options => ({
-  a: {
-    color: oceanBeachTheme.linkColor,
-    textShadow: `none`,
-    textDecoration: `none`,
-    backgroundImage: `none`,
-  },
-  "a:hover": {
-    textDecoration: `underline`,
-  },
-  "h1 a:hover, h1 a:active": {
-    color: options.headerColor,
-    textDecoration: `underline`,
-  },
-})
+
 // @END: Typography Customizations
 
-const typography = new Typography(oceanBeachTheme)
+const typography = new Typography()
 
 export default typography
