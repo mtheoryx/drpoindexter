65425bdc626d6a154eb4523be4e9f6ab156d2820
David Poindexter <drpoindexter@gmail.com>
2021-02-08 16:34:29 -0500

    Add initial light/dark new look

---
+++ public-site/src/components/footer/footer.stories.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Footer from "."
+
+export default {
+  title: "Dashboard/footer",
+}
+
+export const footerLight = () => <Footer theme="light"/>
+
+export const footerDark = () => <Footer theme="dark"/>
