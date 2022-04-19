d3f995d7a4375c570d86bf544769bc34c5c736a4
David Poindexter <drpoindexter@gmail.com>
2022-04-19 14:03:51 -0400

    Cleanup after force delete backend

--- public-site/gatsby-browser.js
+++ public-site/gatsby-browser.js
@@ -1,13 +1,2 @@
-// import Amplify, { Analytics } from "aws-amplify"
-// import awsconfig from "./src/aws-exports"
 //Dark Theme for code block highlighting
 require("prismjs/themes/prism-tomorrow.css")
-
-// Amplify.configure(awsconfig)
-
-// export const onRouteUpdate = ({ location }) => {
-//   Analytics.record({
-//     name: "pageVisit",
-//     attributes: { page: location.pathname },
-//   })
-// }
